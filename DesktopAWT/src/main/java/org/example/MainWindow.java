cd18b4ebd1e201e6bb5dc51f0887f5381e0b13f8
User <User@DESKTOP-0TCDSR6>
2025-04-10 15:20:47 +0300

    AWT form text fields DONE

--- DesktopAWT/src/main/java/org/example/MainWindow.java
+++ DesktopAWT/src/main/java/org/example/MainWindow.java
@@ -1,48 +1,64 @@
 package org.example;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainWindow extends Frame {
+    Label lblLastName   = new Label("Last  name: ");
+    Label lblFirstName  = new Label("First name: ");
+    Label lblBirthDate  = new Label("Birth date: ");
+    Label results = new Label("Results");
+
+    TextField textLastName = new TextField();
+    TextField textFirstName = new TextField();
+    TextField textBirthDate = new TextField();
+
     public MainWindow() throws HeadlessException {
+
         this.setBounds(200,200,400,300);
         this.setTitle("AWT - Abstract Window Toolkit");
 //        Button button = new Button("Button");
 //        button.setBounds(50, 50, 100, 50);
 //        add(button);
-        Label lblLastName   = new Label("Last  name: ");
-        Label lblFirstName  = new Label("First name: ");
-        Label lblBirthDate  = new Label("Birth date: ");
 
-        TextField textLastName = new TextField();
-        TextField textFirstName = new TextField();
-        TextField textBirthDate = new TextField();
 
         Button btnSubmit = new Button("Submit");
         Button btnReset  = new Button("Reset");
 
-        lblLastName.setBounds(50, 50, 100, 32);
-        lblFirstName.setBounds(50, 100, 100, 32);
-        lblBirthDate.setBounds(50, 150, 100, 32);
-
-        textLastName.setBounds(200, 50, 100, 24);
-        textFirstName.setBounds(200, 100, 100, 24);
-        textBirthDate.setBounds(200, 150, 100, 24);
-
-        btnSubmit.setBounds(50, 200, 100, 50);
-        btnReset.setBounds(200, 200, 100, 50);
+//        lblLastName.setBounds(50, 50, 100, 32);
+//        lblFirstName.setBounds(50, 100, 100, 32);
+//        lblBirthDate.setBounds(50, 150, 100, 32);
+//
+//        textLastName.setBounds(200, 50, 100, 24);
+//        textFirstName.setBounds(200, 100, 100, 24);
+//        textBirthDate.setBounds(200, 150, 100, 24);
+//
+//        btnSubmit.setBounds(50, 200, 100, 50);
+//        btnReset.setBounds(200, 200, 100, 50);
 
         this.add(lblLastName);
-        this.add(lblFirstName);
-        this.add(lblBirthDate);
-
         this.add(textLastName);
+
+        this.add(lblFirstName);
         this.add(textFirstName);
+
+        this.add(lblBirthDate);
         this.add(textBirthDate);
 
         this.add(btnSubmit);
         this.add(btnReset);
 
-        this.setLayout(null);
+        this.add(results);
+
+        btnSubmit.addActionListener(this::btnSubmitHandler);
+
+        GridLayout grid = new GridLayout(5,2,10,30);
+        this.setLayout(grid);
         this.setVisible(true);
+    }
+    public void btnSubmitHandler(ActionEvent e)
+    {
+        String results = textLastName.getText() + " " + textFirstName.getText() + " " + textBirthDate.getText();
+        this.results.setText(results);
     }
 }
