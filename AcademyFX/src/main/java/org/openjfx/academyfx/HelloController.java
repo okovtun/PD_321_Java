13e6cbcc4e89c958962c6d29a4e9f3d2a5bd874b
User <User@DESKTOP-0TCDSR6>
2025-04-15 15:53:39 +0300

    loadDirections extra columns

--- AcademyFX/src/main/java/org/openjfx/academyfx/HelloController.java
+++ AcademyFX/src/main/java/org/openjfx/academyfx/HelloController.java
@@ -1,7 +1,12 @@
 package org.openjfx.academyfx;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Callback;
 
 import java.sql.*;
 
@@ -31,6 +36,8 @@ public class HelloController {
     private Button load;
     @FXML
     private Button check;
+    @FXML
+    private TableView tableDirections;
 
     @FXML
     protected void onLoadButtonClick() throws SQLException
@@ -64,5 +71,45 @@ public class HelloController {
         String msg = cbDirections.getValue().toString();
         Alert alert = new Alert(Alert.AlertType.INFORMATION, msg, ButtonType.OK);
         alert.show();
+    }
+    @FXML
+    protected void loadDirections()throws SQLException
+    {
+        String connectionString =
+                "jdbc:sqlserver://localhost:1433;" +
+                        "database=PD_212;"+
+                        "user=PHP;"+
+                        "password=111;"+
+                        "Connect Timeout=10;" +
+                        "TrustServerCertificate=True";
+        Connection connection = DriverManager.getConnection(connectionString);
+        Statement statement = connection.createStatement();
+
+        ResultSet set = statement.executeQuery("SELECT * FROM Directions");
+        for(int i=0; i<set.getMetaData().getColumnCount();i++)
+        {
+            final int j = i;
+            TableColumn col = new TableColumn(set.getMetaData().getColumnName(i+1));
+            col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList,String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                    return new SimpleStringProperty(param.getValue().get(j).toString());
+                }
+            });
+            tableDirections.getColumns().add(col);
+        }
+        ObservableList<ObservableList> data = FXCollections.observableArrayList();
+        while (set.next())
+        {
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for(int i=1;i<=set.getMetaData().getColumnCount(); i++)
+            {
+                row.add(set.getString(i));
+            }
+            data.add(row);
+        }
+        tableDirections.setItems(data);
+
+        connection.close();
     }
 }
